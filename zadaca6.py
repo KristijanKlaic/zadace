fb0e5c269f29c5640640b09959b4c29b202f3088
KristijanKlaic <kristijan.klaic@fpmoz.sum.ba>
2024-08-30 19:16:21 +0200

    Prvi commit

---
+++ zadaca6.py
@@ -0,0 +1,10 @@
+def rekurzija(string):
+    if len(string) == 0:
+        return 
+    else:
+        print(string[-1])
+        rekurzija(string[:-1])
+        
+string = input("Unesite string: ")
+
+rekurzija(string)
---
+++ zadaca8.py
@@ -0,0 +1,4 @@
+from zadaća 6 import ReverseString
+
+a = input("Unesi string i dobićeš njegov reverz: ")
+print("Reverz vašega stringa je", reverz(a))
